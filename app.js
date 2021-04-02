16741086ba28b26d2e2adc3624f30a7b15611ef2
Mrdynamic-soni <ssoni5930@gmail.com>
2021-04-02 09:04:37 +0530

    added hashing

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+// const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -20,7 +20,7 @@ const userSchema =new mongoose.Schema({
     password : String
 });
  
-userSchema.plugin(encrypt,{ secret : process.env.SECRET, encryptedFields : ["password"]});
+//userSchema.plugin(encrypt,{ secret : process.env.SECRET, encryptedFields : ["password"]});
 
 
 const User = new mongoose.model("User",userSchema);
@@ -46,7 +46,7 @@ app.post("/register",function(req,res){
 
     const userx = new User({
         email : req.body.username,
-        password : req.body.password
+        password :md5( req.body.password)
     });
 
     userx.save(function(err){
